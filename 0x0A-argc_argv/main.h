e2e05c64ab84c1d1b013fc3370e4cb534d5d9d12
IanoNjuguna <inc3099@gmail.com>
2023-08-12 21:06:01 +0300

    main.h: create && update

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int prog_name(int argc, char **argv);
+int arg_count(int argc, char **argv);
+
+#endif
