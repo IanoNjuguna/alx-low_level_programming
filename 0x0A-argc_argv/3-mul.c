ea85272cd87611ed881ff23c54ad27c27a98ca7f
IanoNjuguna <inc3099@gmail.com>
2023-08-14 23:21:07 +0300

    3-mul.c: fix how characters in an array & negative ints are treated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+/**
+ * StrIntCheck - Check whether character is a digit (integer)
+ * @str: array of characters
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+bool StrIntCheck(const char *str)
+{
+	int idx;
+
+	for (idx = 0; str[idx] != '\0' && str[idx] != '-'; idx++)
+	{
+		if (!isdigit((unsigned char)str[idx]))
+		{
+			return (false);
+		}
+	}
+	return (true);
+}
 
 /**
  * main - multiplies two numbers
@@ -9,19 +32,24 @@
  * Return: Always 0 (Success)
  * Author: IanoNjuguna
  */
-
 int main(int argc, char *argv[])
 {
 	int temp1, temp2, prod;
 
 	if (argc == 3)
 	{
-		temp1 = atoi(argv[1]);
-		temp2 = atoi(argv[2]);
+		if (StrIntCheck(argv[1]) && StrIntCheck(argv[2]))
+		{	temp1 = atoi(argv[1]);
+			temp2 = atoi(argv[2]);
 
-		prod = temp1 * temp2;
+			prod = temp1 * temp2;
 
-		printf("%d\n", prod);
+			printf("%d\n", prod);
+		}
+		else
+		{
+			printf("Error!\n");
+		}
 	}
 	else
 	{
