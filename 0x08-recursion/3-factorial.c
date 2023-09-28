9e0b7d9316e41694ad6c64a4aa1d54882400770b
IanoNjuguna <inc3099@gmail.com>
2023-09-29 00:56:45 +0300

    3-factorial.c: create file & update with function template main.h: include int factorial(int n)

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ *
+ * @n: stores an integer. We're looking for the factorial of this int
+ * Return: -1 if n < 0
+ *			factorial if n > 0
+ * Author: IanoNjuguna
+ */
+
+int factorial(int n)
+{
+
+}
