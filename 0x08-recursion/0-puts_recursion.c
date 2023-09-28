5bae05cceb13bb5fd457ff15161f05dac21f7511
IanoNjuguna <inc3099@gmail.com>
2023-09-28 18:01:51 +0300

    _putchar.c: create file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,28 @@
 #include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: stores the string to be printed
+ *
+ * Return: void
+ * Author: IanoNjuguna
+ */
+
+void _puts_recursion(char *s)
+{
+	int len = strlen(s);
+
+	return (s[len++]);
+}
+
+/**
+ * main - test the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+
+}
