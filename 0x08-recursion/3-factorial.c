693260d14579b710c125dd5688c31a5d27c67e99
IanoNjuguna <inc3099@gmail.com>
2023-09-29 01:20:22 +0300

    3-factorial.c: function that returns the factorial of a given number. .gitignore: include the test and executable files

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,10 +6,22 @@
  * @n: stores an integer. We're looking for the factorial of this int
  * Return: -1 if n < 0
  *			factorial if n > 0
+ * Factorial of 0 is 1
  * Author: IanoNjuguna
  */
 
 int factorial(int n)
 {
-
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
 }
