afce64356c521e6904a6718919e0088fcc3eb687
IanoNjuguna <inc3099@gmail.com>
2023-09-28 22:59:35 +0300

    1-print_rev_recursion.c: fix the block comment 2-strlen_recursion.c: create file & write function template main.h: update with _strlen_recursion()

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
-/*
- * _print_rev_recursion - print a string in reverse
- * @s: stores the string to be printed
- *
- * Return: void
- * Author: IanoNjuguna
- */
+/**
+* _print_rev_recursion - print a string in reverse using recursion
+*
+* @s: stores the string to be printed
+*
+* Return: void
+* Author: IanoNjuguna
+*/
+
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: stores the string
+ * Return: length of a string
+ * Author: IanoNjuguna
+ */
+
+int _strlen_recursion(char *s)
+{
+
+}
