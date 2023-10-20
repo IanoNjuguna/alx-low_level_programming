ab5fc60569f364d74d0f26f1d67d9f7623a098b3
IanoNjuguna <inc3099@gmail.com>
2023-10-21 02:11:34 +0300

    main.h: update function header - function that prints a struct dog

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -6,18 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * struct dog - information about a dog
- * @name: dog's name
- * @owner: dog's owner
- * @age: dog's age
- */
-struct dog
-{
-	char *name;
-	char *owner;
-	float age;
-};
+void print_dog(struct dog *d);
 
 #endif
 
