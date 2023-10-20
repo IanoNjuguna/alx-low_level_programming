e813baf65769a9c20b0c87b72367ef21c4bc3584
IanoNjuguna <inc3099@gmail.com>
2023-10-21 02:17:25 +0300

    main.h: fix trailing whitespace

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_dog - a function that prints a struct dog
+ * @d: block of memory struct is stored in
+ *
+ * Return: Success or Fail
+ */
+
+void print_dog(struct dog *d)
+{
+	return 
+}
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -9,4 +9,3 @@
 void print_dog(struct dog *d);
 
 #endif
-
